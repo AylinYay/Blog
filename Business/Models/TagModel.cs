ff4da8e22359312fdbdc0138c0b9cc0bb00be334
Aylin ✨ <aylinyay62@gmail.com>
2023-09-07 00:22:11 +0300

    v1

--- Business/Models/TagModel.cs
+++ Business/Models/TagModel.cs
@@ -1,6 +1,7 @@
 #nullable disable
 
 using AppCore.Records.Bases;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Business.Models
@@ -12,5 +13,11 @@ namespace Business.Models
         public string Name { get; set; }
 
         public bool IsPopular { get; set; }
+
+        [DisplayName("Popular")]
+        public string IsPopularDisplay { get; set; }
+
+        [DisplayName("Blog Count")]
+        public int BlogCountDisplay { get; set; }
     }
 }
--- Business/Services/TagService.cs
+++ Business/Services/TagService.cs
@@ -1,5 +1,6 @@
 using AppCore.Business.Services.Bases;
 using AppCore.DataAccess.EntityFramework.Bases;
+using AppCore.Results;
 using AppCore.Results.Bases;
 using Business.Models;
 using DataAccess;
@@ -21,12 +22,21 @@ namespace Business.Services
 
         public Result Add(TagModel model)
         {
-            throw new NotImplementedException();
+            Tag tagEntity = new Tag()
+            {
+                Name = model.Name,
+                IsPopular = model.IsPopular,               
+            };
+
+            _tagRepo.Add(tagEntity);
+
+            return new SuccessResult("Tag added successfully.");
         }
 
         public Result Delete(int id)
         {
-            throw new NotImplementedException();
+            _tagRepo.Delete(id);
+            return new SuccessResult("Tag deleted successfully.");
         }
 
         public void Dispose()
@@ -41,13 +51,27 @@ namespace Business.Services
                 Guid = t.Guid,              
                 Id = t.Id,
                 IsPopular = t.IsPopular,
-                Name = t.Name + " " + (t.IsPopular ? "*" : "")
+                Name = t.Name + (t.IsPopular ? " *" : ""),
+                IsPopularDisplay = t.IsPopular ? "Yes" : "No",
+                BlogCountDisplay = t.BlogTags.Count
             });
         }
 
         public Result Update(TagModel model)
         {
-            throw new NotImplementedException();
+            _tagRepo.Delete<BlogTag>(bt => bt.TagId == model.Id);
+
+            var entity = new Tag()
+            {
+                Name = model.Name,
+                IsPopular = model.IsPopular,
+                Guid = model.Guid,
+                Id = model.Id
+            };
+
+            _tagRepo.Update(entity);
+
+            return new SuccessResult("Tag updated successfully.");
         }
     }
 }
